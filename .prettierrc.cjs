bcc81740b72d1f72f10c422be629921f3282bf1b
k-w-wang <wk980221@icloud.com>
2022-12-15 18:09:55 +0800

    void

--- .prettierrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  // rules: 代码规则
-  rules: {
-    semi: "off", // 行尾必须有分号
-    useTabs: true,
-    tabWidth: 4,
-  },
-};
--- src/App.tsx
+++ src/App.tsx
@@ -3,52 +3,58 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [count, setCount] = useState(2);
+	const [count, setCount] = useState(2);
 
-  useEffect(() => {
-    console.log("useEffect");
-    const promise: Promise<number> = new Promise<number>((resolve, reject) => {
-      resolve(123);
-    });
-    const getPromise: () => Promise<number> = async () => {
-      const num = await promise;
-      // 此处可直接拿到结果
-      console.log("num", num);
-      // return 会当作resolve
-      return num;
-    };
-    const getTwoPromise: () => Promise<number> = async () => {
-      const twoNum = await getPromise();
-      console.log("twoNum", twoNum);
-      return twoNum;
-    };
-    void getTwoPromise();
-  }, []);
+	useEffect(() => {
+		console.log("useEffect");
+		const promise: Promise<number> = new Promise<number>(
+			(resolve, reject) => {
+				resolve(123);
+			}
+		);
+		const getPromise: () => Promise<number> = async () => {
+			const num = await promise;
+			// 此处可直接拿到结果
+			console.log("num", num);
+			// return 会当作resolve
+			return num;
+		};
+		const getTwoPromise: () => Promise<number> = async () => {
+			const twoNum = await getPromise();
+			console.log("twoNum", twoNum);
+			return twoNum;
+		};
+		void getTwoPromise();
+	}, []);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1> Vite + React </h1>
-      <div className="card">
-        <button onClick={() => setCount((count: number) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn m ore
-      </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank" rel="noreferrer">
+					<img
+						src={reactLogo}
+						className="logo react"
+						alt="React logo"
+					/>
+				</a>
+			</div>
+			<h1> Vite + React </h1>
+			<div className="card">
+				<button onClick={() => setCount((count: number) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn m ore
+			</p>
+		</div>
+	);
 };
 
 export default App;
